3edea8870881894bd629ff0ce3ce070fbfa3c09b
Phil Tomson <philtomson@gmail.com>
2014-08-07 14:53:09 -0700

    changed fib_tc.c to work with clang

--- fib_tc.c
+++ fib_tc.c
@@ -1,12 +1,12 @@
 #include <stdint.h>
-uint64_t fib_tc( uint32_t x) {
-  uint64_t fib_rec(uint32_t n, uint64_t acc, uint64_t prev) {
-    if(n == 0) 
-      return acc;
-    else
-      return fib_rec( n-1, acc+prev, acc);
-  }
+uint64_t fib_rec(uint32_t n, uint64_t acc, uint64_t prev) {
+  if(n == 0) 
+    return acc;
+  else
+    return fib_rec( n-1, acc+prev, acc);
+}
 
+uint64_t fib_tc( uint32_t x) {
   if( x < 2)
     return x;
   else
